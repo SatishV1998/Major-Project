9ab39ddb7f7714b5192a21f729142ccd2b2981c6
vanapalli satish <89569677+SatishV1998@users.noreply.github.com>
2023-07-14 18:15:52 +0530

    Update attribute.groovy

--- attribute.groovy
+++ attribute.groovy
@@ -10,18 +10,19 @@ import org.arl.unet.*
   }
 
   final String title = 'Attribute request agent'        
-  final String description = 'Receives attribute requests from other nodes and responds with its attributes, Also updates neighbour attribute values' 
+ 
 
   int delay = 0;   
   LinkedHashMap info = []
-  
-  
-    
-  
+  int ea = 100;
+  int E0=100;
+
+
   @Override
   void startup() {
     
     subscribeForService(Services.DATAGRAM);
+
     def phy = agentForService(Services.PHYSICAL);
   
     
@@ -32,7 +33,7 @@ import org.arl.unet.*
       //periodically broadcast attribute requests with a random delay
       add new TickerBehavior(d, {  
           def mynode = agentForService(Services.NODE_INFO);
-         // System.out.println(mynode.nodeName + ' broadcasted attribute requests');
+          //System.out.println(mynode.nodeName + ' broadcasted attribute requests');
           log.info('--' + mynode.nodeName + ' broadcasted attribute requests')
           
          phy << new ClearReq()
@@ -49,46 +50,49 @@ import org.arl.unet.*
       // respond to protocol USER datagram (Attribute requests)
      
      def node = agentForService(Services.NODE_INFO);
-     //System.out.println(node.nodeName + ' received request from ' + msg.from);
+     System.out.println(node.nodeName + ' received request from ' + msg.from);
      log.info('--' + node.nodeName + ' received request from ' + msg.from )
      
      
      //int lq = Math.random()*100; //link quality
+     int lq
      float distance = Math.sqrt(Math.pow((msg.data[0]-node.location[0]),2) + Math.pow((msg.data[1]-node.location[1]),2) + Math.pow((msg.data[2]-node.location[2]),2))
      
      distance = distance / 1000
-     
-     int Eb = 40 //avg transmission energy per bit
      int N0 = 1 // noise power density
-     int k = 2 //spreading factor
+     int k = 1.5 //spreading factor
      int af = 3 // absorption coefficient for frequency f = 18KHZ
-     int lq = Eb / ( N0 * Math.pow( distance , k ) * Math.pow( af , distance ) ) 
-  
-    
-    
+
      
-     
-     int ea = Math.random()*100 + 1 ; // energy attribute
      int d = Math.random()*5000; //delay
      
      int depth= -1*node.location[2];
      int x = node.location[0];
      int y = node.location[1];
+     float dp = depth/1000
+
+     int Eb=E0  * Math.exp(-af*dp)
      
-     String attstring = lq + ' ' + ea + ' ' + depth + ' ' + x + ' ' + y ;
+     
+     ea=ea * Math.exp(-(0.135) * (dp))
+     lq = Eb / ( N0 * Math.pow( distance , k ) * Math.pow( af , distance ) ) 
+    
       
         add new WakerBehavior(d, {
+         log.info("The EB value :" + Eb + " Link quality:" + lq);
+        log.info("The EA value :" + ea + " from node " + node.nodeName + "Sending to " + msg.from);
+        String attstring = lq + ' ' + ea + ' ' + depth + ' ' + x + ' ' + y
         send new DatagramReq(
           recipient: msg.sender,
           to: msg.from,
           protocol: Protocol.DATA,
-         // data: [lq , ea ,depth] //the three attributes
+         // data: [lq , ea ,depth] //the three attributes    
         data:attstring as byte[]
         )
       })
       ///////
        String att = Integer.toString(lq) + ',' + Integer.toString(ea) + ',' + Integer.toString(depth) + ',' + Integer.toString(x) + ',' + Integer.toString(y);
-       //System.out.println(node.nodeName + ' replied with its attributes ' + att +  ' to ' + msg.from);
+       System.out.println(node.nodeName + ' replied with its attributes ' + att +  ' to ' + msg.from);
        log.info('--' + node.nodeName + ' replied with its attributes ' + att + ' to ' + msg.from);//////
        
       ///////
